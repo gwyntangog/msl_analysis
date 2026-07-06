0eb2578416e8552934dd9973c1f3c5e0b80292e2
Gwyneth Margaux Tangog <gwynt@mit.edu>
2026-07-06 19:39:11 -0400

    integration resolved and tests cleaned

--- iter9_files/functions_test.py
+++ iter9_files/functions_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from functions import tau_callibrate, tau_callibrate_row, tau_callibrate_df, ms_logit, unzero
 from functions import calc_product_cost, get_true_mins_maxes, normalize_attributes, calc_utilities
-from functions import calc_utility_row, normalize_price_row, point_generation_price, point_generation_ratio
+from functions import calc_product_cost_row, normalize_product_cost_row,  calc_utility_row, point_generation_price, generate_graph
 
 class testUnzero(unittest.TestCase):
     """Partitions
@@ -121,19 +121,6 @@ class testNormalizeAttributes(unittest.TestCase):
         print(result)
         pd.testing.assert_frame_equal(result, self.expected_df)
 
-class testPriceNormalize(unittest.TestCase):
-    row = pd.DataFrame([{"weight_attribute_1": 0.3, "weight_attribute_2":0.7,'direction_attribute_1': "positive", 'direction_attribute_2': "negative",
-                                 'cu_attribute_1_value':5,'cu_attribute_2_value':5,
-       'al_attribute_1_value':0,'al_attribute_2_value':0,'attribute_1_min':0,
-       'attribute_1_max':5, 'attribute_2_min':0, 'attribute_2_max':10,
-       "cu_a1_callibrated":1.0, "al_a1_callibrated":0.0,"cu_a2_callibrated":0.5, "al_a2_callibrated":1.0}]).iloc[0]
-    def test_price_normalize(self):
-        result = normalize_price_row(self.row, price = 2)
-        expected = 0.6
-        result = round(result,1)
-        self.assertEqual(result, expected)
-
-
 
 class testUtilityCalcs(unittest.TestCase):
     """
@@ -154,20 +141,6 @@ class testUtilityCalcs(unittest.TestCase):
         result = calc_utilities(self.test_df,self.num_test_atts)
         print(result)
         pd.testing.assert_frame_equal(result, self.expected_df)
-    def test_utility_row1(self):
-        row = self.test_df.iloc[0]
-        result_cu = calc_utility_row(row, num_attributes = self.num_test_atts)
-        result_al = calc_utility_row(row, variable = "al",num_attributes = self.num_test_atts)
-        result_cu = round(result_cu,2)
-        result_al = round(result_al, 2)
-        self.assertEqual(0.65, result_cu)
-        self.assertEqual(0.70, result_al)
-    def test_utility_row2(self):
-        row = self.test_df.iloc[0]
-        result_cu = calc_utility_row(row, price= 1, num_attributes = self.num_test_atts)
-        result_cu = round(result_cu,2)
-        self.assertEqual(0.59, result_cu)
-
 
 ####
 
@@ -211,38 +184,11 @@ class testTauFunctions(unittest.TestCase):
         self.assertEqual(result, expected)
 
 ######################
-class testPointGeneration(unittest.TestCase):
-    """
-    """
-    test_df = pd.DataFrame([{"region": "India","weight_attribute_1": 0.3, "weight_attribute_2":0.7,'direction_attribute_1': "positive", 'direction_attribute_2': "negative",
-                                 'cu_attribute_1_value':5,'cu_attribute_2_value':5,
-       'al_attribute_1_value':0,'al_attribute_2_value':0,'attribute_1_min':0,
-       'attribute_1_max':5, 'attribute_2_min':0, 'attribute_2_max':10,
-       "cu_a1_callibrated":1.0, "al_a1_callibrated":0.0,"cu_a2_callibrated":0.5, "al_a2_callibrated":1.0, "al_utility":0.2, "tau_value":0.2}])
-    def test_point_generation(self):
-        result = point_generation_price(self.test_df, "India", price_range = [1,2], variable = "cu", num_attributes = 2)
-        expected = [0.88, 0.84]
-        self.assertEqual(round(result[0],2), expected[0])
-        self.assertEqual(round(result[1],2), expected[1])
+
+# write tests for the following: calc_product_cost_row, normalize_product_cost_row,  calc_utility_row, point_generation_price, generate_graph
+
 
 ######################
-class testPointGenerationRatio(unittest.TestCase):
-    """
-    """
-    test_df = pd.DataFrame([{"region": "India","weight_attribute_1": 0.3, "weight_attribute_2":0.7,'direction_attribute_1': "positive", 'direction_attribute_2': "negative",
-                                 'cu_attribute_1_value':5,'cu_attribute_2_value':5,
-       'al_attribute_1_value':0,'al_attribute_2_value':0,'attribute_1_min':0,
-       'attribute_1_max':5, 'attribute_2_min':0, 'attribute_2_max':10,
-       "cu_a1_callibrated":1.0, "al_a1_callibrated":0.0,"cu_a2_callibrated":0.5, "al_a2_callibrated":1.0, "al_utility":0.2, "tau_value":0.2}])
-    def test_point_generation_ratio(self):
-        result = point_generation_ratio(self.test_df, "India", ratio_range =[1,2], num_attributes = 2)
-        # al_price = 2
-        # al_utility = 0.6*0.3+ 1*0.7 = 0.88
-        # cu_prices = 2,4 (0.6, 0.2)
-        # cu_utility =0.6*0.3 + 0.5*0.7 = 0.53, 0.2*0.3 + 0.5*0.7 = 0.41
-        expected = [0.148, 0.087]
-        self.assertEqual(round(result[0],3), expected[0])
-        self.assertEqual(round(result[1],3), expected[1])
 
 if __name__ == '__main__':
     unittest.main()
