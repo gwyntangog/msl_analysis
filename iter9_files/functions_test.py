b9f2d6a68c41145e7f40c4d1a4cf0ea5c285ae0d
Gwyneth Margaux Tangog <gwynt@mit.edu>
2026-07-09 01:10:02 -0400

    add tests

--- iter9_files/functions_test.py
+++ iter9_files/functions_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from functions import tau_callibrate, tau_callibrate_row, tau_callibrate_df, ms_logit, unzero
 from functions import calc_product_cost, get_true_mins_maxes, normalize_attributes, calc_utilities
-from functions import calc_product_cost_row, normalize_product_cost_row,  calc_utility_row, point_generation_price, generate_graph
+from functions import calc_product_cost_row, normalize_product_cost_row,  calc_utility_row, point_generation_price, point_generation_ratio
 
 class testUnzero(unittest.TestCase):
     """Partitions
@@ -263,9 +263,116 @@ class testNormalizeProductCostRow(unittest.TestCase):
         expected = round(5/9,2)
         self.assertEqual(result, expected)
 
+class testUtilityRow(unittest.TestCase):
+    """
+    """
+    row =  pd.DataFrame([{"weight_attribute_1": 0.3, "weight_attribute_2":0.7,'direction_attribute_1': "positive", 'direction_attribute_2': "negative",
+                                 'cu_attribute_1_value':5,'cu_attribute_2_value':5,
+       'al_attribute_1_value':2,'al_attribute_2_value':2,'attribute_1_min':0,
+       'attribute_1_max':5, 'attribute_2_min':10, 'attribute_2_max':10,
+       "cu_a1_callibrated":1.0, "al_a1_callibrated":0.4,"cu_a2_callibrated":0.5, "al_a2_callibrated":1.0,
+       "cu_non_material_cost_per_unit":1, "cu_copper_kg":2, "cu_aluminum_kg": 2,
+       "al_non_material_cost_per_unit":1, "al_copper_kg":1, "al_aluminum_kg": 1,
+       "copper_price_per_kg": 1, "aluminum_price_per_kg":1}]).iloc[0]
+    def test_utility_row1(self):
+        result = calc_utility_row(self.row, num_attributes = 2)
+        result = round(result,2)
+        expected = round(0.35,2)
+        self.assertEqual(result, expected)
+    def test_utility_row2(self):
+        result = calc_utility_row(self.row, variable = "al", num_attributes = 2)
+        result = round(result,2)
+        expected = round(0.82,2)
+        self.assertEqual(result, expected)
 
+class testPointGenPrice(unittest.TestCase):
+    """
+    """
+    df =  pd.DataFrame([{"region": "India","weight_attribute_1": 0.3, "weight_attribute_2":0.7,'direction_attribute_1': "positive", 'direction_attribute_2': "negative",
+                                 'cu_attribute_1_value':5,'cu_attribute_2_value':5,
+       'al_attribute_1_value':2,'al_attribute_2_value':2,'attribute_1_min':0,
+       'attribute_1_max':20, 'attribute_2_min':10, 'attribute_2_max':10,
+       "cu_a1_callibrated":1.0, "al_a1_callibrated":0.4,"cu_a2_callibrated":0.5, "al_a2_callibrated":1.0,
+       "cu_non_material_cost_per_unit":1, "cu_copper_kg":2, "cu_aluminum_kg": 2,
+       "al_non_material_cost_per_unit":1, "al_copper_kg":1, "al_aluminum_kg": 1,
+       "copper_price_per_kg": 1, "aluminum_price_per_kg":1, "tau_value":1}])
+    def test_point_gen1(self):
+        result = point_generation_price(self.df, region = "India", price_range = [3,5], num_attributes = 2)
+        result = np.round(result,2)
+        result = result.tolist()
+        # cu cost = 1 + 6 + 2 = 9 = , al cost = 1 + 3 + 1 = 5
+        expected = [0.40,0.39] #0.35
+        self.assertListEqual(result, expected)
 
-# write tests for the following: calc_utility_row, point_generation_price, point_generation_ratio
+class testPointGenRatio(unittest.TestCase):
+    """
+    """
+    df =  pd.DataFrame([{"region": "India","weight_attribute_1": 0.3, "weight_attribute_2":0.7,'direction_attribute_1': "positive", 'direction_attribute_2': "negative",
+                                 'cu_attribute_1_value':5,'cu_attribute_2_value':5,
+       'al_attribute_1_value':2,'al_attribute_2_value':2,'attribute_1_min':0,
+       'attribute_1_max':20, 'attribute_2_min':10, 'attribute_2_max':10,
+       "cu_a1_callibrated":1.0, "al_a1_callibrated":0.4,"cu_a2_callibrated":0.5, "al_a2_callibrated":1.0,
+       "cu_non_material_cost_per_unit":1, "cu_copper_kg":2, "cu_aluminum_kg": 2,
+       "al_non_material_cost_per_unit":1, "al_copper_kg":1, "al_aluminum_kg": 1,
+       "copper_price_per_kg": 1, "aluminum_price_per_kg":1, "tau_value":1}])
+    def test_point_ratio1(self):
+        result = point_generation_ratio(self.df, region = "India", ratio_range = [1,2], num_attributes = 2)
+        result = np.round(result,2)
+        result = result.tolist()
+        expected = [0.40,0.39]
+        self.assertListEqual(result, expected)
+
+# def point_generation_price(input_df, region, price_range = np.arange(0,5, 0.01), variable="cu", num_attributes = 5):
+#     """
+#     return a series of points. Assumes only one row with the region value.
+#     """
+#     ms_points = []
+#     region_row = input_df.loc[input_df["region"] == region].iloc[0]
+#     tau = region_row["tau_value"]
+#     if variable == "cu":
+#         for price in price_range:
+#             al_utility = calc_utility_row(region_row, cu_material_cost = price, num_attributes=num_attributes, variable = "al")
+#             cu_utility = calc_utility_row(region_row,cu_material_cost = price, num_attributes = num_attributes, variable = "cu")
+#             ms = ms_logit(cu_utility, al_utility, tau)
+#             ms_points.append(ms)
+#     elif variable == "al":
+#         for price in price_range:
+#             al_utility = calc_utility_row(region_row, al_material_cost = price, num_attributes=num_attributes, variable = "al")
+#             cu_utility = calc_utility_row(region_row, al_material_cost = price, num_attributes = num_attributes, variable = "cu")
+#             ms = ms_logit(cu_utility, al_utility, tau)
+#             ms_points.append(ms)
+#     else:
+#         raise ValueError("Invalid input")
+#     return ms_points
+
+# def point_generation_ratio(input_df, region, hold = "al", hold_value = 2,ratio_range =np.arange(0.1,2, 0.01),num_attributes=5):
+#     """
+#     Change ratios.
+#     """
+#     ms_points = []
+#     region_row = input_df.loc[input_df["region"] == region].iloc[0]
+#     tau = region_row["tau_value"]
+#     if hold == "al":
+#         for ratio in ratio_range:
+#             al_material_cost = hold_value
+#             cu_material_cost = hold_value*ratio
+#             al_utility = calc_utility_row(region_row, cu_material_cost = cu_material_cost, al_material_cost=al_material_cost, num_attributes=num_attributes, variable = "al")
+#             cu_utility = calc_utility_row(region_row,cu_material_cost = cu_material_cost, al_material_cost = al_material_cost, num_attributes = num_attributes, variable = "cu")
+#             ms = ms_logit(cu_utility, al_utility, tau)
+#             ms_points.append(ms)
+#     elif hold == "cu":
+#         for ratio in ratio_range:
+#             al_material_cost = hold_value/ratio
+#             cu_material_cost = hold_value
+#             al_utility = calc_utility_row(region_row, cu_material_cost = cu_material_cost, al_material_cost=al_material_cost, num_attributes=num_attributes, variable = "al")
+#             cu_utility = calc_utility_row(region_row,cu_material_cost = cu_material_cost, al_material_cost = al_material_cost, num_attributes = num_attributes, variable = "cu")
+#             ms = ms_logit(cu_utility, al_utility, tau)
+#             ms_points.append(ms)
+#     else:
+#         raise ValueError("Invalid input")
+#     return ms_points
+
+# write tests for the following: point_generation_price, point_generation_ratio
 
 ######################
 
